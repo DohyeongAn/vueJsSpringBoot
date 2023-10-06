bdca108dc74cf44cc365598cefeed70a84d4358d
DohyeongAn <rrgg0204@naver.com>
2023-10-06 17:51:06 +0900

    Spting Boot 초기설정 완료

---
+++ vueBoard/src/main/java/com/example/vueBoard/VueBoardApplication.java
@@ -0,0 +1,14 @@
+package com.example.vueBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBoardApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VueBoardApplication.class, args);
+	}
+
+}
---
+++ vueBoard/src/main/java/com/example/vueBoard/VueBoardController.java
@@ -0,0 +1,24 @@
+package com.example.vueBoard;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+@RestController
+public class VueBoardController {
+//    @GetMapping("/test")
+//    public Map<String, Object> getTest() {
+//        Map<String, Object> resultMap = new HashMap<>();
+//        return resultMap;
+//    }
+//    @RequestMapping(value = "/")
+//    public String index() {
+//
+//        return "/index";
+//    }
+
+
+}
